c4e59285eb73a2bde5e1b10b46a9d502f1c6d35b
Timothée Monin <timothee@monin.net>
2026-05-14 16:30:22 +0200

    Try and fix a SyntaxError bug

--- model.py
+++ model.py
@@ -20,7 +20,7 @@ class GPTConfig:
     n_layer: int = 12  # number of layers
     n_head: int = 12  # number of heads
     n_embd: int = 768  # embedding dimension
-    use_swiglu: bool = True  # whether to use SwiGLU instead of GELU
+    use_swiglu: bool = True  # whether to use SwiGLU instead of GELU
     use_rope: bool = True  # whether to use RoPE instead of learning positional embeddings
     use_rmsnorm: bool = True  # whether to use RMSNorm instead of Layer Normalization
 
