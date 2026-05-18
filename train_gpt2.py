67bce698a3acd82b65ca4b27056bbe78be3bb24e
Timothée Monin <timothee@monin.net>
2026-05-18 19:40:40 +0200

    Lower min training LR

--- train_gpt2.py
+++ train_gpt2.py
@@ -122,7 +122,7 @@ if ddp:
 raw_model = model.module if ddp else model  # always contains the "raw" unwrapped model
 
 max_lr = args.max_lr  # defaults to 6e-4
-min_lr = max_lr * 0.1
+min_lr = max_lr * 0.05
 warmup_steps = args.warmup_steps  # defaults to 200
 max_steps = args.num_steps  # 19,073 steps is ~1 epoch, if data is 10B tokens and batch size 0.5M tokens
 def get_lr(it):
